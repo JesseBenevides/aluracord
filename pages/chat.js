423cd3bab73d7feffdbcc69b8891e74e6a8a82cc
Jesse Benevides <jessebenevides@gmail.com>
2022-01-26 23:08:46 -0300

    Adiciona página do chat

---
+++ pages/chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from '../components/Title';
+
+function chat() {
+  return (
+    <div>
+      <h2>Página do Chat</h2>
+    </div>
+  );
+}
+
+export default chat;
