119f1c85df58150eb55cbb4f4bd2dc8d4dc6ea55
Jesse Benevides <jessebenevides@gmail.com>
2022-01-30 20:47:52 -0300

    Adiciona o Supabase ao projeto

---
+++ services/supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzU4NDY1NCwiZXhwIjoxOTU5MTYwNjU0fQ.qlF7iDuxQrKj97hj54wXjkgIjh-r-BU6gHchY7Pr7z0';
+
+const SUPABASE_URL = 'https://dqkrvqsrotcnqcvzqnfj.supabase.co';
