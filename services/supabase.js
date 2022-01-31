c37a99e494e4461e086e99e65374aaa780b2cbf9
Jesse Benevides <jessebenevides@gmail.com>
2022-01-30 21:43:20 -0300

    Adiciona serviços de get e post do supabase

--- services/supabase.js
+++ services/supabase.js
@@ -1,3 +1,24 @@
+import { createClient } from '@supabase/supabase-js';
+
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzU4NDY1NCwiZXhwIjoxOTU5MTYwNjU0fQ.qlF7iDuxQrKj97hj54wXjkgIjh-r-BU6gHchY7Pr7z0';
 
 const SUPABASE_URL = 'https://dqkrvqsrotcnqcvzqnfj.supabase.co';
+
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export async function fetchMessages() {
+  const { data } = await supabaseClient
+    .from('messages')
+    .select('*')
+    .order('id', { ascending: false });
+
+  return data;
+}
+
+export async function sendMessages(message) {
+  const { data } = await supabaseClient
+    .from('messages')
+    .insert([message]);
+  
+    return data[0];
+}
